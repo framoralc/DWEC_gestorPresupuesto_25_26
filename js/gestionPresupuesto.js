9c32c70deeb9a38a2ed42653c925a7ad61e95951
framoralc <framoralc@alu.edu.gva.es>
2025-09-23 20:22:00 +0200

    Creación de la variable actualizarPresupuesto()

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,10 +1,19 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(valor) {
     // TODO
+    let presupuesto = 0;
+    if(valor >= 0){
+        presupuesto = valor;
+        return presupuesto;
+    }
+    else{
+        alert("El valor introducido es incorrecto");
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
