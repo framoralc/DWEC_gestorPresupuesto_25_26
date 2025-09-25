3d19442ce30f4624148616c6dc55b5e6bed9cf5b
framoralc <framoralc@alu.edu.gva.es>
2025-09-25 18:15:45 +0200

    Creación de variables globales

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,6 +2,8 @@
 "use strict";
 // TODO: Variable global
 let presupuesto = 0;
+let idGasto = 0;
+let gastos = [];
 
 function actualizarPresupuesto(valor) {
     if(valor >= 0 && !isNaN(valor)){
@@ -46,11 +48,23 @@ function CrearGasto(descripcion, valor) {
     }
 }
 
+function listarGastos(){
+
+}
+
+function anyadirGasto(){
+
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
