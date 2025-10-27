ce71bfc0bc1360fbb68b68f65e0a444f3df7091e
framoralc <framoralc@alu.edu.gva.es>
2025-10-27 19:14:09 +0100

    Se ha creado la función mostrarGastoWeb()

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,3 +1,24 @@
-import * as gp from './gestionPresupuesto';
-import * as gpw from './gestionPresupuestoWeb';
+import * as gp from './gestionPresupuesto.js';
+import * as gpw from './gestionPresupuestoWeb.js';
 
+//presupuesto
+
+let valor = 1500;
+gp.actualizarPresupuesto(valor);
+gpw.mostrarDatoEnId("presupuesto", gp.mostrarPresupuesto())
+
+//gastos
+
+let gasto1 = new gp.CrearGasto("Comprar carne", 23.44, "2021-10-06", "casa", "comida");
+let gasto2 = new gp.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+let gasto3 = new gp.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
+let gasto4 = new gp.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+let gasto5 = new gp.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+let gasto6 = new gp.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+
+gpw.mostrarGastoWeb("listado-gastos-completo", gasto1);
+gpw.mostrarGastoWeb("listado-gastos-completo", gasto2);
+gpw.mostrarGastoWeb("listado-gastos-completo", gasto3);
+gpw.mostrarGastoWeb("listado-gastos-completo", gasto4);
+gpw.mostrarGastoWeb("listado-gastos-completo", gasto5);
+gpw.mostrarGastoWeb("listado-gastos-completo", gasto6);
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,10 +1,47 @@
+'use strict';
+
 function mostrarDatoEnId(idElemento, valor){
+    let text = document.createElement("p");
+    text.textContent = valor;
     let id = document.getElementById(idElemento);
-    id.innerHTML(valor);
+    id.appendChild(text);
 }
 
 function mostrarGastoWeb(idElemento, gasto){
+    let id = document.getElementById(idElemento);
+    let classGasto = document.createElement("div");
+    classGasto.classList.add("gasto");
 
+    let gastoDescripcion = document.createElement("div");
+    gastoDescripcion.classList.add("gasto-descripcion");
+    gastoDescripcion.textContent = gasto.descripcion;
+
+    let gastoFecha = document.createElement("div");
+    gastoFecha.classList.add("gasto-fecha");
+    gastoFecha.textContent = gasto.obtenerPeriodoAgrupacion("dia");
+
+    let gastoValor = document.createElement("div");
+    gastoValor.classList.add("gasto-valor");
+    gastoValor.textContent = gasto.valor;
+
+    let gastoListaEtiquetas = document.createElement("div");
+    gastoListaEtiquetas.classList.add("gasto-etiqueta")
+
+    let gastoEtiquetas = document.createElement("span");
+    gastoEtiquetas.classList.add("gasto-etiqueta-etiquetas");
+
+    
+    classGasto.appendChild(gastoDescripcion);
+    classGasto.appendChild(gastoFecha);
+    classGasto.appendChild(gastoValor);
+    classGasto.appendChild(gastoListaEtiquetas);
+
+    for(let i = 0; i < gasto.etiquetas.length; i++){
+        gastoEtiquetas.textContent = gasto.etiquetas
+        gastoListaEtiquetas.appendChild(gastoEtiquetas)
+    }
+
+    id.appendChild(classGasto);
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup){
